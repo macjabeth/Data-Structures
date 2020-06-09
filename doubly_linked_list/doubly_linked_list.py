7cbd3f04fd9226b5611ca57cee92416c933983ac
macjabeth <macbethjonathan@gmail.com>
2020-06-09 18:44:43 -0400

    complete doubly linked list

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -10,8 +10,12 @@ class ListNode:
     after this node. Note that this node could already
     have a next node it is point to."""
     def insert_after(self, value):
+        if not isinstance(value, ListNode):
+            value = ListNode(value)
         current_next = self.next
-        self.next = ListNode(value, self, current_next)
+        value.prev = self
+        value.next = current_next
+        self.next = value
         if current_next:
             current_next.prev = self.next
 
@@ -19,8 +23,12 @@ class ListNode:
     before this node. Note that this node could already
     have a previous node it is point to."""
     def insert_before(self, value):
+        if not isinstance(value, ListNode):
+            value = ListNode(value)
         current_prev = self.prev
-        self.prev = ListNode(value, current_prev, self)
+        value.prev = current_prev
+        value.next = self
+        self.prev = value
         if current_prev:
             current_prev.next = self.prev
 
@@ -48,41 +56,91 @@ class DoublyLinkedList:
     as the new head of the list. Don't forget to handle 
     the old head node's previous pointer accordingly."""
     def add_to_head(self, value):
-        pass
+        if not isinstance(value, ListNode):
+            value = ListNode(value, None, self.head)
+
+        if not self.head:
+            self.head = value
+            self.tail = value
+        else:
+            self.head.insert_before(value)
+            self.head = value
+
+        self.length += 1
 
     """Removes the List's current head node, making the
     current head's next node the new head of the List.
     Returns the value of the removed Node."""
     def remove_from_head(self):
-        pass
+        current_head = self.head
+        self.delete(self.head)
+        return current_head.value
 
     """Wraps the given value in a ListNode and inserts it 
     as the new tail of the list. Don't forget to handle 
     the old tail node's next pointer accordingly."""
     def add_to_tail(self, value):
-        pass
+        if not isinstance(value, ListNode):
+            value = ListNode(value, self.tail)
+        
+        if not self.tail:
+            self.tail = value
+            self.head = value
+        else:
+            self.tail.insert_after(value)
+            self.tail = value
+
+        self.length += 1
 
     """Removes the List's current tail node, making the 
     current tail's previous node the new tail of the List.
     Returns the value of the removed Node."""
     def remove_from_tail(self):
-        pass
+        current_tail = self.tail
+        self.delete(self.tail)
+        return current_tail.value
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List."""
     def move_to_front(self, node):
-        pass
+        self.delete(node)
+        node.next = self.head
+        node.prev = None
+        self.add_to_head(node)
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
     def move_to_end(self, node):
-        pass
+       self.delete(node)
+       node.next = None
+       node.prev = self.tail
+       self.add_to_tail(node)
 
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
     def delete(self, node):
-        pass
+        if node is self.head:
+            self.head = node.next
+        if node is self.tail:
+            self.tail = node.prev
+
+        node.delete()
+
+        self.length -= 1
+
+        if self.length < 0:
+            self.length = 0
         
     """Returns the highest value currently in the list"""
     def get_max(self):
-        pass
+        if not self.head:
+            return None
+
+        node = self.head
+        current_max = node.value
+
+        while node is not None:
+            current_max = max(current_max, node.value)
+            node = node.next
+
+        return current_max
