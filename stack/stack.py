cdac9947f6fe644584959a05071eb294d764bde3
macjabeth <macbethjonathan@gmail.com>
2020-06-12 11:00:44 -0400

    update stack

--- stack/stack.py
+++ stack/stack.py
@@ -47,9 +47,9 @@ class Stack:
         self.size += 1
 
     def pop(self):
-        self.size -= 1
+        if self.size == 0:
+            return None
 
-        if self.size < 0:
-            self.size = 0
+        self.size -= 1
 
         return self.storage.remove_head()
